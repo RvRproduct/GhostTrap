a62743612116e92254c17552f471ee77e2b8f8db
Roberto Valentino Reynoso <68643525+RvRproduct@users.noreply.github.com>
2025-01-08 15:31:19 -0700

    Starting

---
+++ GhostTrap/Source/GhostTrap/GhostTrap.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GhostTrap.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GhostTrap, "GhostTrap" );
